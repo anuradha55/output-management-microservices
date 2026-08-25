1f91dc442d37f6e01e2842c63667966459a47719
anuradha55 <anu.gujar35@gmail.com>
2026-08-25 13:22:12 +0530

    feat: add Kafka retry and dead letter recovery

---
+++ enrichment-service/src/main/java/com/output/management/enrichment/config/KafkaRetryConfig.java
@@ -0,0 +1,20 @@
+package com.output.management.enrichment.config;
+
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaRetryConfig {
+    @Bean
+    DefaultErrorHandler kafkaErrorHandler(KafkaTemplate<Object, Object> template) {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(template,
+                (record, ex) -> new TopicPartition(record.topic() + ".DLT", record.partition()));
+        return new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 3L));
+    }
+}
