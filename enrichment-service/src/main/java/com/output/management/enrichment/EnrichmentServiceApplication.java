86b320279486f673f45025030d77292a2cfe71bd
anuradha55 <anu.gujar35@gmail.com>
2026-08-25 12:56:20 +0530

    Add enrichment service application

---
+++ enrichment-service/src/main/java/com/output/management/enrichment/EnrichmentServiceApplication.java
@@ -0,0 +1,5 @@
+package com.output.management.enrichment;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class EnrichmentServiceApplication { public static void main(String[] args) { SpringApplication.run(EnrichmentServiceApplication.class, args); } }
